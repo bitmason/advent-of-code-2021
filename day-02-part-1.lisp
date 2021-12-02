6603a45d03883ac996dec7e64888c59af5cb3f82
Darren Stone <dstone@bitmason.com>
2021-12-01 23:59:40 -0800

    Update day-02-part-1.lisp

--- day-02-part-1.lisp
+++ day-02-part-1.lisp
@@ -3,6 +3,13 @@
 ;;;; Solution by Darren Stone
 ;;;; <dstone@bitmason.com>
 
+(defun day-02-part-1 (input)
+  (let* ((hor-deps (mapcar 'cmd-to-hor-dep input))
+         (hors (mapcar 'car hor-deps))
+         (deps (mapcar 'second hor-deps))
+         (final-pos (list (reduce '+ hors) (reduce '+ deps))))
+    (reduce '* final-pos)))
+
 ;;; given cmd, return delta for (hor dep)
 ;;; e.g. "forward 5" => (5 0), "up 2" => (0 -2)
 (defun cmd-to-hor-dep (cmd)
@@ -10,10 +17,3 @@
     ((list "forward" n) (list (parse-integer n) 0))
     ((list "up" n) (list 0 (- (parse-integer n))))
     ((list "down" n) (list 0 (parse-integer n)))))
-
-(defun day-02-part-1 (input)
-  (let* ((hor-deps (mapcar 'cmd-to-hor-dep input))
-         (hors (mapcar 'car hor-deps))
-         (deps (mapcar 'second hor-deps))
-         (final-pos (list (reduce '+ hors) (reduce '+ deps))))
-    (reduce '* final-pos)))
