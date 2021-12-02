5402103d2526fb4077e2484f5cd66439f3aab88d
Darren Stone <dstone@bitmason.com>
2021-12-01 23:48:53 -0800

    Add files via upload

---
+++ test.lisp
@@ -0,0 +1,19 @@
+;;;; Advent of Code 2021, Day 2
+;;;; https://adventofcode.com/2021/day/2
+;;;; Solution by Darren Stone
+;;;; <dstone@bitmason.com>
+
+;;; given cmd, return delta for (hor dep)
+;;; e.g. "forward 5" => (5 0), "up 2" => (0 -2)
+(defun cmd-to-hor-dep (cmd)
+  (match (words cmd)
+    ((list "forward" n) (list (parse-integer n) 0))
+    ((list "up" n) (list 0 (- (parse-integer n))))
+    ((list "down" n) (list 0 (parse-integer n)))))
+
+(defun day-02-part-1 (input)
+  (let* ((hor-deps (mapcar 'cmd-to-hor-dep input))
+         (hors (mapcar 'car hor-deps))
+         (deps (mapcar 'second hor-deps))
+         (final-pos (list (reduce '+ hors) (reduce '+ deps))))
+    (reduce '* final-pos)))
