2e0e91f2e4d86e2931b23b4f251456eee9026509
Darren Stone <dstone@bitmason.com>
2021-12-02 00:00:05 -0800

    Update day-02-part-2.lisp

--- day-02-part-2.lisp
+++ day-02-part-2.lisp
@@ -3,6 +3,10 @@
 ;;;; Solution by Darren Stone
 ;;;; <dstone@bitmason.com>
 
+(defun day-02-part-2 (input)
+  (reduce '* (butlast (reduce 'state-cmd-apply input
+                              :initial-value '(0 0 0)))))
+
 ;;; given state (hor dep aim), apply cmd & return new state
 ;;; e.g. (1 2 3) "forward 5" => (6 17 3), (1 2 3) "up 2" => (1 2 1)
 (defun state-cmd-apply (state cmd)
@@ -13,7 +17,3 @@
         ("forward" (list (+ hor n) (+ dep (* aim n)) aim))
         ("up" (list hor dep (- aim n)))
         ("down" (list hor dep (+ aim n)))))))
-
-(defun day-02-part-2 (input)
-  (reduce '* (butlast (reduce 'state-cmd-apply input
-                              :initial-value '(0 0 0)))))
