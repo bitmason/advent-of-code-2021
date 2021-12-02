e7026f7c49fd0658494c1dd74bc6405060520010
Darren Stone <dstone@bitmason.com>
2021-12-01 21:00:51 -0800

    new structure & rename

---
+++ day01.lisp
@@ -0,0 +1,14 @@
+; Advent of Code 2021, Day 1
+; https://adventofcode.com/2021/day/1
+; Solution by Darren Stone <dstone@bitmason.com>
+
+(defun day-01 (input)
+  (let* ((depths (mapcar 'parse-integer input))
+
+         (depth-diffs (mapcar '- (cdr depths) depths))
+
+         (window-sums (mapcar '+ depths (cdr depths) (cddr depths)))
+         (window-sum-diffs (mapcar '- (cdr window-sums) window-sums)))
+
+    ((length (remove-if-not 'plusp depth-diffs)) ; part 1
+     (length (remove-if-not 'plusp window-sum-diffs))) ; part 2
