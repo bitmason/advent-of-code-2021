d4a9ce4eafb6fdf55ae3f8e256e282c34dbf2404
Darren Stone <dstone@bitmason.com>
2021-12-03 20:46:23 -0800

    Update day-03-part-1.lisp

--- day-03-part-1.lisp
+++ day-03-part-1.lisp
@@ -1,4 +1,4 @@
-;;;; Advent of Code 2021, Day 3, Part 1
+;;;; Advent of Code 2021, Day 3
 ;;;; https://adventofcode.com/2021/day/3
 ;;;; Solution by Darren Stone
 ;;;; <dstone@bitmason.com>
@@ -12,6 +12,12 @@
          (epsilon-rate (logxor (- (expt 2 (length (car input))) 1) gamma-rate)))
     (* gamma-rate epsilon-rate)))
 
+(defun day-03-part-2 (input)
+  (* (parse-integer (car (reduce #'o2-nums (loop for pos below (length (car input))
+                                                 collect pos) :initial-value input)) :radix 2)
+     (parse-integer (car (reduce #'co2-nums (loop for pos below (length (car input))
+                                                  collect pos) :initial-value input)) :radix 2)))
+
 (defun nth-bit-set-p (n str) (eq #\1 (nth n (coerce str 'list)))) ; nth char is #\1 ?
 
 (defun lines-by-nth-bit (n lines) ; split to pair of lists: (lines-with-nth-bit-0 lines-with-nth-bit-1)
@@ -19,3 +25,15 @@
         (remove-if (lambda (str) (not (nth-bit-set-p n str))) lines)))
 
 (defun chars-to-int (chars) (parse-integer (coerce chars 'string) :radix 2)) ; e.g. (#\1 #\0 #\1) => 5
+
+(defun o2-nums (nums pos) ; drop nums with minority bit at pos
+  (let* ((by-bit (lines-by-nth-bit pos nums))
+         (nums-0 (car by-bit))
+         (nums-1 (cadr by-bit)))
+    (if (eq 1 (length nums)) nums (if (> (length nums-0) (length nums-1)) nums-0 nums-1))))
+
+(defun co2-nums (nums pos) ; drop nums with majority bit at pos
+  (let* ((by-bit (lines-by-nth-bit pos nums))
+         (nums-0 (car by-bit))
+         (nums-1 (cadr by-bit)))
+    (if (eq 1 (length nums)) nums (if (<= (length nums-0) (length nums-1)) nums-0 nums-1))))
