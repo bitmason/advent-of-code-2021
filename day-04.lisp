c587325e84b68d5fcd9733b24d8e18ec963c4b46
Darren Stone <dstone@bitmason.com>
2021-12-04 10:44:28 -0800

    Update day-04.lisp

--- day-04.lisp
+++ day-04.lisp
@@ -2,27 +2,24 @@
 ;;;; https://adventofcode.com/2021/day/4
 ;;;; Solution by Darren Stone <dstone@bitmason.com>
 
-(defun day-04-part-1 (input)
-  (let ((deck (mapcar #'parse-integer (split "," (car input)))))
-    (loop named top for drawn in (loop for n from 1 to (length deck) collect (subseq deck 0 n))
-          do (loop for board in (bingo-boards input)
-                   when (board-wins board drawn)
-                     do (return-from top (board-score board drawn))))))
+(defun day-04-part-1 (input) (cadr (first (all-wins input))))
 
-(defun day-04-part-2 (input)
-  (let* ((deck (mapcar #'parse-integer (split "," (car input))))
-         (wins (loop for drawn in (loop for n from 1 to (length deck) collect (subseq deck 0 n))
-                     append (loop for board in (bingo-boards input)
-                                   when (board-wins board drawn)
-                                     collect (list board (board-score board drawn))))))
-    (cadar (last (remove-duplicates wins :from-end t :test (lambda (a b) (equal (car a) (car b))))))))
+(defun day-04-part-2 (input) (cadar (last (all-wins input))))
+
+(defun all-wins (input) ; list of all (board score) wins in drawn order
+  (let ((deck (mapcar #'parse-integer (split "," (car input)))))
+    (remove-duplicates (loop for drawn in (loop for n from 1 to (length deck) collect (subseq deck 0 n))
+                             append (loop for board in (bingo-boards input)
+                                          when (board-wins-p board drawn)
+                                            collect (list board (board-score board drawn))))
+                       :from-end t :test (lambda (a b) (equal (car a) (car b))))))
 
 (defun bingo-boards (input) ; list of 25-element lists of integers
   (let* ((flat (mapcar #'parse-integer (words (join " " (cdr input)))))
          (num-boards (/ (length flat) 25)))
     (loop for n below num-boards collect (subseq flat (* n 25) (+ 25 (* n 25))))))
 
-(defun board-wins (board drawn) ; given drawn numbers, board wins?
+(defun board-wins-p (board drawn) ; given drawn numbers, board wins?
   (let* ((lines (append (loop for r in '(0 5 10 15 20) collect (loop for v from r to (+ r 4) collect v))
                         (loop for c in '(0 1 2 3 4) collect (loop for v from c to (+ c 20) by 5 collect v))))
          (line-nums (loop for line in lines collect (mapcar (lambda (pos) (nth pos board)) line))))
