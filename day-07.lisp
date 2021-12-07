7436fc0898e61257ddfad137bf0b3543dd2098d1
Darren Stone <dstone@bitmason.com>
2021-12-06 22:43:45 -0800

    decluttered reduce/mapcar/loops

--- day-07.lisp
+++ day-07.lisp
@@ -5,25 +5,21 @@
 (defun day-07 (input)
   (let ((crabs (mapcar #'parse-integer (split "," (car input)))))
     (mapcar (lambda (fuel-calc) (min-fuel fuel-calc crabs))
-            (list 'fuel-calc-part-1 
+            (list 'fuel-calc-part-1
                   'fuel-calc-part-2))))
 
 (defun min-fuel (fuel-calc crabs) ; minimum fuel to an aligned destination
-  (reduce #'min
-          (mapcar (lambda (dest) (apply fuel-calc (list dest crabs)))
-                  (loop for dest
-                        from (reduce #'min crabs)
-                          to (reduce #'max crabs)
-                        collect dest))))
+  (loop for dest
+        from (reduce #'min crabs)
+          to (reduce #'max crabs)
+        minimize (apply fuel-calc (list dest crabs))))
 
 (defun fuel-calc-part-1 (dest crabs) ; total fuel to move all crabs to dest (part 1)
-  (reduce #'+
-          (mapcar (lambda (crab) (abs (- crab dest)))
-                  crabs)))
+  (loop for crab in crabs
+        sum (abs (- crab dest))))
 
 (defun fuel-calc-part-2 (dest crabs) ; total fuel to move all crabs to dest (part 2)
-  (reduce #'+
-          (mapcar (lambda (crab)
-                    (loop for n to (abs (- crab dest))
-                          sum n))
-                  crabs)))
+  (loop for crab in crabs
+        sum (/ (* (abs (- crab dest))
+                  (+ 1 (abs (- crab dest))))
+               2)))
